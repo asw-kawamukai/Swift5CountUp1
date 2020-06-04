0589935a64b42f49743e28557e24c729defec133
川向　亮 <r.kawamukai@a-sw.jp>
2020-06-04 17:39:17 +0900

    First Commit

--- Swift5CountUp1/ViewController.swift
+++ Swift5CountUp1/ViewController.swift
@@ -9,12 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var countLabel: UILabel!
+    var count = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        countLabel.text = "0"
     }
 
-
+    @IBAction func plusButton(_ sender: Any) {
+        count = count + 1
+        countLabel.text = String(count)
+        
+        if count >= 10 {
+            changewTextColor()
+        }
+    }
+    
+    @IBAction func minusButton(_ sender: Any) {
+        count = count - 1
+        countLabel.text = String(count)
+        
+        if count <= 0 {
+            resetColor()
+        }
+    }
+    
+    func changewTextColor() {
+        countLabel.textColor = .yellow
+        
+    }
+    func resetColor() {
+        countLabel.textColor = .white
+    }
 }
 
